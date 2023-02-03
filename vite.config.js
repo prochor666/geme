097f4a0bea4e35d426ed7ef6bf21625d4184ed00
Jan Prochazka <jan.prochazka@softino.cz>
2023-02-03 18:17:06 +0100

    Add image parts

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,10 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
-import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        chunkSplitPlugin({
-            strategy: 'unbundle',
-            customSplitting: {
-                // All files in `src/container` will be merged together in `container` chunk
-                composables: [/src\/composables/],
-                components: [/src\/components/],
-                models: [/src\/models/],
-                pages: [/src\/pages/],
-                i18n: [/src\/i18n/],
-            },
-        }),
-        splitVendorChunkPlugin(),
-    ],
+    plugins: [vue(), splitVendorChunkPlugin()],
     server: {
-        host: 'tplui.test',
+        host: 'geme.test',
         port: 3000,
     },
     build: {
